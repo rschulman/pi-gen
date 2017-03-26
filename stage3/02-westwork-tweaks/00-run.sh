76ab29334ef75a13de00730c6959c4865ec02423
Ross Schulman <ross@rbs.io>
2017-03-25 21:56:02 -0400

    Update run.sh to use /data directory

--- stage3/02-westwork-tweaks/00-run.sh
+++ stage3/02-westwork-tweaks/00-run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
 # Get Rainloop and set permissions
-install -v -d                               ${ROOTFS_DIR}/var/www
-curl https://www.rainloop.net/repository/webmail/rainloop-community-latest.zip > ${ROOTFS_DIR}/var/www/
-unzip ${ROOTFS_DIR}/var/www/rainloop-community-latest.zip
-rm ${ROOTFS_DIR}/var/www/rainloop-community-latest.zip
-cd ${ROOTFS_DIR}/var/www/rainloop
+install -v -d                               ${ROOTFS_DIR}/data/www
+curl https://www.rainloop.net/repository/webmail/rainloop-community-latest.zip > ${ROOTFS_DIR}/data/www/
+unzip ${ROOTFS_DIR}/data/www/rainloop-community-latest.zip
+rm ${ROOTFS_DIR}/data/www/rainloop-community-latest.zip
+cd ${ROOTFS_DIR}/data/www/rainloop
 find . -type d -exec chmod 755 {} \;
 find . -type f -exec chmod 644 {} \;
 chown -R www-data:www-data .
